7ab8f5c83b41d75d5449286e1b7a1ed1531fb662
zxinyu <quarktest@outlook.com>
2020-04-16 18:52:18 +0800

    select.vue的使用

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,46 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <todo-list>
+      <todo-item @delete="handleDelete" v-for ="item in list" :key="item.title" :title="item.title" :del="item.del">
+        <template v-slot:pre-icon ="{value}">
+          <span>前置图标{{value}}</span>
+          <select>111 </select>
+
+        </template>
+
+      </todo-item>
+    </todo-list>
   </div>
+  
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import TodoList from './components/TodoList.vue'
+import TodoItem from './components/TodoItem.vue'
+import select from './components/select.vue'
+
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    TodoList,TodoItem,select
+  },
+  data(){
+    return{
+      message:'Hello,World',
+      list:[{title:"课程1",
+            del:false},
+            {title:"课程2",
+            del:true}],
+    }
+  },
+  methods:{
+    handleDelete(val){
+      console.log('handleDelete',val)
+    }
+  
   }
+
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
