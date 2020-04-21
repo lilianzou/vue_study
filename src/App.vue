593f318fe85c7bb0ee155e304169eb36128a1e10
lillian <307314362@qq.com>
2020-04-21 22:23:47 +0800

    11

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,26 @@
 <template>
+
   <div id="app">
-    <cardtest></cardtest>
+    <a-button></a-button>
+    
+  
+    <selectvue></selectvue>
+    <checkbox></checkbox>
+
   </div>
   
 </template>
 
 <script>
-import cardtest from './components/cardtest.vue'
+import selectvue from './components/selectvue.vue'
+import checkbox from './components/CheckBox.vue'
+
+
 
 export default {
   name: 'App',
   components: {
-    cardtest
+    selectvue,checkbox
   },
   data(){
     return{
